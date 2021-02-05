c2c629a6d463936b60a1a6be177847ad68259354
vmrosa <47866729+vmrosa@users.noreply.github.com>
2021-02-05 15:23:28 -0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,29 @@
+provider "helm" {
+  kubernetes {
+    load_config_file       = false
+    cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
+    host                   = var.kubernetes_cluster_endpoint
+    exec {
+      api_version = "client.authentication.k8s.io/v1alpha1"
+      command     = "aws-iam-authenticator"
+      args        = ["token", "-i", "${var.kubernetes_cluster_name}"]
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "helm_release" "traefik-ingress" {
+  name       = "ms-traefik-ingress"
+  chart      = "traefik"
+  repository = "https://helm.traefik.io/traefik"
+  values = [<<EOF
+  service:
+    annotations:
+      service.beta.kubernetes.io/aws-load-balancer-type: nlb
+      externalTrafficPolicy: Local
+  EOF
+  ]
+}
